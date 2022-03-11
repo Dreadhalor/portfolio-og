da34326815ffd2adca873135816d1be0a5614e9f
Dreadhalor <Scotthetrick2@yahoo.com>
2022-03-10 21:50:18 -0800

    Fixed Tailwind not purging unused classes from css output

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.{html,ts}"],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
