92ee976f2213f4900cfec8cc03238f9206465fd2
Dreadhalor <Scotthetrick2@yahoo.com>
2022-03-15 01:30:26 -0700

    Tried to make the physics independent of framerate - I was wrong Sorta kinda recovered & now it's roughly okay again

--- frontend/src/app/components/navbar-two/navbar-two.component.ts
+++ frontend/src/app/components/navbar-two/navbar-two.component.ts
@@ -12,22 +12,27 @@ export class NavbarTwoComponent implements OnInit, OnDestroy {
   private border = 1;
   private offset = 0;
   private velocity = 0;
-  private damping = 0.88;
+  private damping = 0.95;
+  private c = 0.2;
 
   private dragstart: number | null = null;
 
   private lastMouseMove: PointerEvent | null = null;
+  private currentMouseMove: PointerEvent | null = null;
+
+  private currentMouseTick: PointerEvent | null = null;
+  private lastMouseTick: PointerEvent | null = null;
 
   constructor(private site: SiteService) {}
 
   ngOnInit(): void {
     requestAnimationFrame(this.tick);
-    window.addEventListener('pointerup', this.pointerup);
-    window.addEventListener('pointermove', this.pointermove);
+    document.addEventListener('pointerup', this.pointerup);
+    document.addEventListener('pointermove', this.pointermove);
   }
   ngOnDestroy(): void {
-    window.removeEventListener('pointerup', this.pointerup);
-    window.removeEventListener('pointermove', this.pointermove);
+    document.removeEventListener('pointerup', this.pointerup);
+    document.removeEventListener('pointermove', this.pointermove);
   }
 
   getSideLength() {
@@ -49,75 +54,154 @@ export class NavbarTwoComponent implements OnInit, OnDestroy {
     return this.site.getTestData();
   }
 
-  setMouseEvent(event: PointerEvent) {}
+  queuePointerMove(event: PointerEvent) {
+    this.lastMouseMove = this.currentMouseMove;
+    // this.lastMouse = this.currentMouse;
+    this.currentMouseMove = event;
+    // this.currentMouse = event;
+  }
+  tickPointer() {
+    this.lastMouseTick = this.currentMouseTick;
+    this.currentMouseTick = this.currentMouseMove;
+    console.log('tickDX: ' + this.getTickDX());
+  }
+  tickPointerMove() {
+    this.lastMouseMove = this.currentMouseMove;
+    this.currentMouseMove = null;
+  }
+  resetPointerMoves() {
+    this.lastMouseMove = null;
+    this.currentMouseMove = null;
+  }
 
   pointerdown(event: PointerEvent) {
     // this.setMouseEvent(event);
     // this.mouseEvent = event;
     this.dragstart = event.clientX - this.offset;
+    this.velocity = 0;
   }
   pointermove = (event: PointerEvent) => {
     // this.setMouseEvent(event);
-    this.lastMouseMove = event;
     if (this.dragstart !== null) {
+      // console.log(event.getCoalescedEvents());
       // console.log(this.dragstart);
+      this.queuePointerMove(event);
       this.offset = event.clientX - this.dragstart;
+      // console.log('offset: ' + this.offset);
     }
+    // else this.lastMouseMove = null;
   };
   pointerup = (event: PointerEvent) => {
     // this.setMouseEvent(event);
     this.setVelocity(event);
+    this.resetPointerMoves();
     this.dragstart = null;
-    // console.log('window mouseup, dragstart: ' + this.dragstart);
+    console.log('window mouseup, dragstart: ' + this.dragstart);
   };
-  rightdown(event: Event) {
-    console.log(event);
-    this.velocity += 10;
-  }
-  leftdown(event: Event) {
-    console.log(event);
-    this.velocity -= 10;
-  }
 
-  // setVelocity() {
-  //   console.log(this.mouseEvent);
-  //   console.log(this.prevMouseEvent);
-  //   let x1 = this.mouseEvent?.clientX ?? 0;
-  //   let x2 = this.prevMouseEvent?.clientX ?? 0;
-  //   let result = x1 - x2;
-  //   this.velocity = result;
-  //   console.log(this.velocity);
-  //   this.tickVelocity(null);
-  // }
   setVelocity = (mouseup: PointerEvent) => {
-    // if (this.lastMouseMove) {
-    // let delta_t = mouseup.timeStamp - (this.lastMouseMove?.timeStamp ?? 0);
-    // console.log('delta_t: ' + delta_t);
-    let delta_x = this.lastMouseMove?.movementX ?? 0;
-    // console.log('delta_x: ' + delta_x);
-    let v = delta_x;
-    this.velocity = v;
-    // this.tickVelocity(null);
-    // console.log(v);
-    // }
+    this.velocity = this.calculateTickVelocity();
   };
+  calculateTickVelocity() {
+    if (!this.currentMouseTick || !this.lastMouseTick) return 0;
+    if (this.currentMouseTick === this.lastMouseTick) return 0;
+    let dx = this.getTickDX();
+    // console.log('dx: ' + dx);
+    let dt = this.getTickDT();
+    // console.log('dt: ' + dt);
+    let v = dx;
+    // console.log('v: ' + v);
+    return v;
+  }
+  getTickDX() {
+    if (this.currentMouseTick && this.lastMouseTick) {
+      let x1 = this.currentMouseTick?.clientX;
+      let x2 = this.lastMouseTick?.clientX;
+      return Math.round(x1 - x2);
+    }
+    return 0;
+  }
+  getTickDT() {
+    if (this.currentMouseTick && this.lastMouseTick) {
+      let t1 = this.currentMouseTick?.timeStamp;
+      let t2 = this.lastMouseTick?.timeStamp;
+      return (t1 - t2) / 100;
+    }
+    return 1;
+  }
+  calculateVelocity() {
+    if (!this.currentMouseMove || !this.lastMouseMove) return 0;
+    if (this.currentMouseMove === this.lastMouseMove) return 0;
+    let dx = this.getDX();
+    console.log('dx: ' + dx);
+    let dt = this.getDT();
+    console.log('dt: ' + dt);
+    let v = dx;
+    console.log('v: ' + v);
+    return v;
+  }
+  getDX() {
+    if (this.currentMouseMove && this.lastMouseMove) {
+      let x1 = this.currentMouseMove?.clientX;
+      let x2 = this.lastMouseMove?.clientX;
+      return x1 - x2;
+    }
+    return 0;
+  }
+  getDT() {
+    if (this.currentMouseMove && this.lastMouseMove) {
+      let t1 = this.currentMouseMove?.timeStamp;
+      let t2 = this.lastMouseMove?.timeStamp;
+      return (t1 - t2) / 300;
+    }
+    return 1;
+  }
   private timestamp = 0;
-  tick = (time: any) => {
-    // console.log('time: ' + time);
-    console.log('velocity: ' + this.velocity);
-    console.log('offset: ' + this.offset);
 
-    if (this?.velocity ?? 0 !== 0) {
-      let delta = 0;
-      if (time) delta = time - this.timestamp;
-      // console.log('delta: ' + delta);
+  getDelta(t1: number, t2: number) {
+    let delta_ms = t2 - t1;
+    // console.log(delta_ms);
+    let delta_s = delta_ms / 1000;
+    // console.log(delta_s);
+    return delta_s;
+  }
+
+  tickAcceleration() {}
+  tickVelocity = (delta: number) => {
+    let absolute_dv = this.velocity * this.c;
+    let tick_dv = absolute_dv * delta;
+    this.velocity -= tick_dv;
+  };
+  tickPosition = (delta: number) => {
+    let tick_dx = this.velocity * delta;
+    this.offset += tick_dx;
+  };
+
+  tick = (time: number) => {
+    // console.log('time: ' + time);
+    // console.log('velocity: ' + this.velocity);
+    // console.log('offset: ' + this.offset);
+    this.tickPointer();
+    if (this.velocity !== 0) {
+      let delta = this.getDelta(this.timestamp, time);
+      // console.log('new delta: ' + delta);
       this.timestamp = time;
+      // this.tickVelocity(delta);
+      // this.tickPosition(delta);
+
+      // let dv = this.velocity * this.c * delta;
+      // this.velocity -= dv;
+
       // this.offset += Math.floor((this.velocity * delta) / 1000);
       this.offset += this.velocity;
+      // this.offset += this.velocity;
+      // console.log(this.offset);
+      // this.velocity *= this.damping * delta;
       this.velocity *= this.damping;
       if (Math.abs(this.velocity) < 0.1) this.velocity = 0;
-      // console.log(this.velocity);
+      // console.log('tick-end velocity: ' + this.velocity);
     }
+    // this.tickPointerMove();
     requestAnimationFrame(this.tick);
   };
 }
