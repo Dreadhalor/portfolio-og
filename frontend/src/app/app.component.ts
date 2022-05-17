960070b41f813a1671dd4ff58e1f360915ac405f
Dreadhalor <Scotthetrick2@yahoo.com>
2022-05-16 19:35:41 -0500

    Added favicons, including a neat toggling favicon for Chrome that detects light/dark mode

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -16,6 +16,26 @@ export class AppComponent {
     this.index = index;
   }
 
+  checkDarkMode() {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches;
+  }
+
+  setFavicons() {
+    let favicon = document.getElementById('favicon') as HTMLLinkElement;
+    // let apple_touch_icon = document.getElementById(
+    //   'apple-touch-icon'
+    // ) as HTMLLinkElement;
+    if (this.checkDarkMode()) {
+      if (favicon) favicon.href = '/icons/favicon-dark.svg';
+      // if (apple_touch_icon)
+      //   apple_touch_icon.href = '/icons/apple-touch-icon-dark.png';
+    } else {
+      if (favicon) favicon.href = '/icons/favicon.svg';
+      // if (apple_touch_icon)
+      //   apple_touch_icon.href = '/icons/apple-touch-icon.png';
+    }
+  }
+
   constructor() {
     // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
     let vh = window.innerHeight * 0.01;
@@ -28,5 +48,11 @@ export class AppComponent {
       let vh = window.innerHeight * 0.01;
       document.documentElement.style.setProperty('--vh', `${vh}px`);
     });
+
+    this.setFavicons();
+
+    window
+      .matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener('change', (e) => this.setFavicons());
   }
 }
